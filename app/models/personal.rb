7765e03449ea5fc74d815209641a0ebac1b8879a
daniel_wheels <djmarin-weels@hotmail.com>
2015-05-26 19:41:26 +0000

    se cambiaron por h4 los nombres de las celdas ya tiene relleno editar, new, index

--- app/models/personal.rb
+++ app/models/personal.rb
@@ -9,7 +9,7 @@ class Personal < ActiveRecord::Base
     #validacion de atributos unicos
     validates :cedula, uniqueness: { message: "No se puede Repetir" }
     validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i,
-    message: 'Formato no valido' }
+   message: 'Formato no valido' }
 
     
     
@@ -21,7 +21,7 @@ class Personal < ActiveRecord::Base
     
     def self.search(search, page)
         where(['upper(placa) like ?',
-        "%#{search}%".upcase]).paginate(page: page, per_page: 200000).order("placa")
+        "%#{search}%".upcase]).paginate(page: page, per_page: 2200000).order("placa")
     end
 	
 end
